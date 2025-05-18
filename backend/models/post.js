d4313a66c9f7d4916e974a74a0df13dc2acbb45d
madgepereira020701 <madgepereira020701@gmail.com>
2025-05-18 18:10:18 +0530

    Modified post.js

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,11 +1,28 @@
+// const mongoose = require("mongoose");
+
+// const postSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   content: { type: String, required: true },
+//   image: {type: String, default: ""},
+//   author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//   createdAt: { type: Date, default: Date.now },
+// });
+
+// module.exports = mongoose.model("posts", postSchema);
+
+
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  image: {type: String, default: ""},
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    image: { type: String, default: "" },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  {
+    timestamps: { createdAt: 'createdAt', updatedAt: 'lastModifiedDate' }
+  }
+);
 
 module.exports = mongoose.model("posts", postSchema);
