76fed3e95da1a40394e7ad99d2884f803790ec88
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-10-31 11:40:46 +0530

    Comment out password update and reset functions
    Comment out the updatePassword function and related logic, and the generatePasswordResetToken function. Also comment out the sendPasswordResetEmail function and its associated logic.

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
-const FRONTEND_URL = process.env.FRONTEND_URL;
+// const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const Admin = require("../models/auth");
 
@@ -63,147 +63,147 @@ const userlogin = async (req, res) => {
   }
 };
 
-const updatePassword = async (req, res) => {
-  const { token } = req.params;
-  const { newpassword, confirmPassword } = req.body;
+// const updatePassword = async (req, res) => {
+//   const { token } = req.params;
+//   const { newpassword, confirmPassword } = req.body;
 
-  try {
-    if (!newpassword || !confirmPassword) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "All fields are required" });
-    }
+//   try {
+//     if (!newpassword || !confirmPassword) {
+//       return res
+//         .status(400)
+//         .json({ isSuccess: false, message: "All fields are required" });
+//     }
 
-    if (newpassword !== confirmPassword) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "Passwords do not match" });
-    }
+//     if (newpassword !== confirmPassword) {
+//       return res
+//         .status(400)
+//         .json({ isSuccess: false, message: "Passwords do not match" });
+//     }
 
-    const foundUser = await Admin.findOne({
-      passwordResetToken: token,
-      passwordResetExpires: { $gt: Date.now() },
-    });
+//     const foundUser = await Admin.findOne({
+//       passwordResetToken: token,
+//       passwordResetExpires: { $gt: Date.now() },
+//     });
 
-    if (!foundUser) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "Invalid or expired token" });
-    }
+//     if (!foundUser) {
+//       return res
+//         .status(400)
+//         .json({ isSuccess: false, message: "Invalid or expired token" });
+//     }
 
-    const isMatch = await bcrypt.compare(
-      newpassword.trim(),
-      foundUser.password
-    );
-    if (isMatch) {
-      return res.status(400).json({
-        isSuccess: false,
-        message: "New password cannot be the same as old password",
-      });
-    }
+//     const isMatch = await bcrypt.compare(
+//       newpassword.trim(),
+//       foundUser.password
+//     );
+//     if (isMatch) {
+//       return res.status(400).json({
+//         isSuccess: false,
+//         message: "New password cannot be the same as old password",
+//       });
+//     }
 
-    foundUser.password = newpassword.trim();
-    foundUser.passwordResetToken = undefined;
-    foundUser.passwordResetExpires = undefined;
-    await foundUser.save();
+//     foundUser.password = newpassword.trim();
+//     foundUser.passwordResetToken = undefined;
+//     foundUser.passwordResetExpires = undefined;
+//     await foundUser.save();
 
-    return res
-      .status(200)
-      .json({ isSuccess: true, message: "Password updated successfully" });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .json({ isSuccess: false, message: "An error occured" });
-  }
-};
+//     return res
+//       .status(200)
+//       .json({ isSuccess: true, message: "Password updated successfully" });
+//   } catch (err) {
+//     console.error(err);
+//     return res
+//       .status(500)
+//       .json({ isSuccess: false, message: "An error occured" });
+//   }
+// };
 
-function generatePasswordResetToken() {
-  const token = jwt.sign({ purpose: "passwordReset" }, JWT_SECRET, {
-    expiresIn: "1hr",
-  });
-  return token;
-}
+// function generatePasswordResetToken() {
+//   const token = jwt.sign({ purpose: "passwordReset" }, JWT_SECRET, {
+//     expiresIn: "1hr",
+//   });
+//   return token;
+// }
 
-async function storeToken(email, token) {
-  try {
-    const user = await Admin.findOne({ email: email });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "User not found" });
-    }
-    user.passwordResetToken = token;
-    user.passwordResetExpires = Date.now() + 3600000;
-    await user.save();
+// async function storeToken(email, token) {
+//   try {
+//     const user = await Admin.findOne({ email: email });
+//     if (!user) {
+//       return res
+//         .status(400)
+//         .json({ isSuccess: false, message: "User not found" });
+//     }
+//     user.passwordResetToken = token;
+//     user.passwordResetExpires = Date.now() + 3600000;
+//     await user.save();
 
-    return res
-      .status(200)
-      .json({ isSuccess: true, message: "Token stored successfully" });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .json({ isSuccess: false, message: "An error occured" });
-  }
-}
+//     return res
+//       .status(200)
+//       .json({ isSuccess: true, message: "Token stored successfully" });
+//   } catch (err) {
+//     console.error(err);
+//     return res
+//       .status(500)
+//       .json({ isSuccess: false, message: "An error occured" });
+//   }
+// }
 
-const passwordresetrequest = async (req, res) => {
-  const { email } = req.body;
-  try {
-    const admin = await Admin.findOne({ email });
-    if (!admin) {
-      return res
-        .status(400)
-        .json({ isSuccess: false, message: "Admin not found" });
-    }
-    const token = generatePasswordResetToken();
+// const passwordresetrequest = async (req, res) => {
+//   const { email } = req.body;
+//   try {
+//     const admin = await Admin.findOne({ email });
+//     if (!admin) {
+//       return res
+//         .status(400)
+//         .json({ isSuccess: false, message: "Admin not found" });
+//     }
+//     const token = generatePasswordResetToken();
 
-    try {
-      await sendPasswordResetEmail(email, token);
-      return res.status(200).json({
-        isSuccess: true,
-        message: "Password reset email sent successfully",
-      });
-    } catch (err) {
-      console.error(err);
-      return res
-        .status(500)
-        .json({ isSuccess: false, message: "An error occured" });
-    }
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .json({ isSuccess: false, message: "An error occured" });
-  }
-};
+//     try {
+//       await sendPasswordResetEmail(email, token);
+//       return res.status(200).json({
+//         isSuccess: true,
+//         message: "Password reset email sent successfully",
+//       });
+//     } catch (err) {
+//       console.error(err);
+//       return res
+//         .status(500)
+//         .json({ isSuccess: false, message: "An error occured" });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     return res
+//       .status(500)
+//       .json({ isSuccess: false, message: "An error occured" });
+//   }
+// };
 
-const nodemailer = require("nodemailer");
-async function sendPasswordResetEmail(email, token) {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: '',
-      pass: '', // Use an app-specific password for Gmail
-      },
-  });
+// const nodemailer = require("nodemailer");
+// async function sendPasswordResetEmail(email, token) {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: '',
+//       pass: '', // Use an app-specific password for Gmail
+//       },
+//   });
 
-  const resetLink = `${FRONTEND_UR}/changepassword?token=${token}`;
+//   const resetLink = `${FRONTEND_URL}/changepassword?token=${token}`;
 
-  const mailOptions = {
-    from: "madgepereira020701@gmail.com",
-    to: email,
-    subject: "jhhh",
-    html: `
-             <p> 
-             < a href="${resetLink}" >Reset Password</a> 
-             </p>
-            `,
-  };
+//   const mailOptions = {
+//     from: "madgepereira020701@gmail.com",
+//     to: email,
+//     subject: "jhhh",
+//     html: `
+//              <p> 
+//              < a href="${resetLink}" >Reset Password</a> 
+//              </p>
+//             `,
+//   };
 
-  await transporter.sendMail(mailOptions);
-  await storeToken(email, token);
-}
+//   await transporter.sendMail(mailOptions);
+//   await storeToken(email, token);
+// }
 
 module.exports = { userregister, userlogin, updatePassword, passwordresetrequest };
