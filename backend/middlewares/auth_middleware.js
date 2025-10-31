dfbc89d1608afd068650d06da4855bbdd817bbca
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-10-31 11:41:51 +0530

    Use environment variable for JWT secret
    Replaced hardcoded JWT secret with environment variable.

--- backend/middlewares/auth_middleware.js
+++ backend/middlewares/auth_middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'mysecretkey';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Middleware to verify token
 const protect = async (req, res, next) => {
